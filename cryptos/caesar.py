cb03dcf0c1aabea4e03bc2d52f4974fe6553f17b
Divyanshu Kakwani <divkakwani@gmail.com>
2016-02-22 14:43:25 +0530

    Implement Encryptor and Decryptor classes for Caesar cipher

---
+++ cryptos/caesar.py
@@ -0,0 +1,25 @@
+from abstract import AbstractEncryptor, AbstractDecryptor
+from string import ascii_lowercase
+
+
+class CaesarEncryptor(AbstractEncryptor):
+
+    def __init__(self, shift):
+        self.key = shift
+
+    def encrypt(self, message):
+        pt = message.lower()
+        ctl = map(lambda c: chr((ord(c)-ord('a')+self.key)%26+ord('a')), pt)
+        ct = ''.join(ctl)
+        return ct
+
+
+class CaesarDecryptor(AbstractDecryptor):
+
+    def __init__(self, shift):
+        self.key = shift
+
+    def decrypt(self, ciphertext):
+        ptl = map(lambda c: chr((ord(c)-ord('a')-self.key)%26+ord('a')), ciphertext)
+        pt = ''.join(ptl)
+        return pt
